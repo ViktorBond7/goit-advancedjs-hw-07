5e97f8878b1d0f1be7a074749f33edbcb7681130
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2025-11-09 10:18:53 +0100

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  tenants: Person[] = [];
+  door: boolean = false;
+
+  constructor(public key: Key) {}
+
+  comeIn(key: Person): void {
+    if (this.door) {
+      this.tenants.push(key);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відкрито");
+    } else {
+      console.log("Невірний ключ!");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
